fe85a10ad3e972f3e116dc568fbf1d0657df3ee8
James Brennan <james@jamesbrennan.ca>
2016-02-01 09:37:43 -0800

    State dumping and loading

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Todo MVC Rx vitual-dom
 
+## Development
+
+1. `npm start`
+2. Go to: [localhost:8080](http://localhost:8080)
+
 ## Event Stream Recording and Playback
 
 ```
@@ -15,4 +20,17 @@ app.playRecording(recording);
 app.playRecording(recording, {realtime: false}); // Play recorded events immediately
 
 app.playRecording(recording, {speed: 2}); // Play recording at 2x speed
+```
+
+## State Snapshotting
+
+```
+// Interact with UI ...
+
+// window.app
+var state = app.currentState();
+
+// `JSON.stringify(state)` and copy `state` to another tab (or refresh the current tab)
+
+app.replaceState(state);
 ```
--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ class Todo {
     this.container = container;
     this.eventBus = new Rx.Subject();
     this.recording = null;
+    this.state = null;
+    this.stateSnapshot = null;
   }
 
   startRecording() {
@@ -51,6 +53,15 @@ class Todo {
     );
   }
 
+  currentState() {
+    this.emit({action: 'stateSnapshot'});
+    return this.stateSnapshot.asMutable();
+  }
+
+  replaceState(state) {
+    this.emit({action: 'replaceState', state: state});
+  }
+
   emit(event) {
     return this.eventBus.onNext(Immutable(event));
   }
@@ -59,60 +70,65 @@ class Todo {
     let initial = Immutable({
           show: 'all',
           todos: {}
-        }),
-        viewEvents = this.eventBus.scan((acc, event) => {
-          var result = null;
-          switch(event.action) {
-          case 'create':
-            return acc.setIn(['todos', genId()], {
-              text: event.text,
-              editing: false,
-              completed: false
-            });
-          case 'edit':
-            return acc.setIn(['todos', event.id, 'editing'], true);
-          case 'unedit':
-            return acc.setIn(['todos', event.id, 'editing'], false);
-          case 'save':
-            return acc.setIn(['todos', event.id, 'editing'], false)
-              .setIn(['todos', event.id, 'text'], event.text);
-          case 'uncomplete':
-            return acc.setIn(['todos', event.id, 'completed'], false);
-          case 'complete':
-            return acc.setIn(['todos', event.id, 'completed'], true);
-          case 'destroy':
-            return acc.setIn(['todos'], acc.todos.without([event.id]));
-          case 'clearCompleted':
-            let removeIds = _.flatMap(_.toPairs(acc.todos), ([id, todo]) => {
-              if(todo.completed) {
-                return [id];
-              } else {
-                return [];
-              }
-            });
-            return acc.setIn(['todos'], acc.todos.without(removeIds));
-          case 'toggelAll':
-            return acc.setIn(['todos'], Immutable(_.toPairs(acc.todos)).map(([id, todo]) => {
-              return [id, todo.set('completed', event.completed)];
-            }).asObject());
-          case 'show':
-            return acc.setIn(['show'], event.show);
-          default:
-            return acc;
+        });
+
+    this.state = this.eventBus.scan((acc, event) => {
+      switch(event.action) {
+      case 'create':
+        return acc.setIn(['todos', genId()], {
+          text: event.text,
+          editing: false,
+          completed: false
+        });
+      case 'edit':
+        return acc.setIn(['todos', event.id, 'editing'], true);
+      case 'unedit':
+        return acc.setIn(['todos', event.id, 'editing'], false);
+      case 'save':
+        return acc.setIn(['todos', event.id, 'editing'], false)
+          .setIn(['todos', event.id, 'text'], event.text);
+      case 'uncomplete':
+        return acc.setIn(['todos', event.id, 'completed'], false);
+      case 'complete':
+        return acc.setIn(['todos', event.id, 'completed'], true);
+      case 'destroy':
+        return acc.setIn(['todos'], acc.todos.without([event.id]));
+      case 'clearCompleted':
+        let removeIds = _.flatMap(_.toPairs(acc.todos), ([id, todo]) => {
+          if(todo.completed) {
+            return [id];
+          } else {
+            return [];
           }
-        }, initial);
+        });
+        return acc.setIn(['todos'], acc.todos.without(removeIds));
+      case 'toggelAll':
+        return acc.setIn(['todos'], Immutable(_.toPairs(acc.todos)).map(([id, todo]) => {
+          return [id, todo.set('completed', event.completed)];
+        }).asObject());
+      case 'show':
+        return acc.setIn(['show'], event.show);
+      case 'stateSnapshot':
+        this.stateSnapshot = acc;
+        return acc;
+      case 'replaceState':
+        return event.state;
+      default:
+        return acc;
+      }
+    }, initial);
 
     // if debug
     this.eventBus.subscribe((e) => { console.debug('[Commands]', e); },
                             (e) => { console.error('[Commands]', e); },
                             () => { console.debug('[Commands]', 'Completed'); });
 
-    viewEvents.subscribe((e) => { console.debug('[View]', e); },
-                         (e) => { console.error('[View]', e); },
-                         () => { console.debug('[View]', 'Completed'); });
+    this.state.subscribe((e) => { console.debug('[View]', e); },
+                              (e) => { console.error('[View]', e); },
+                              () => { console.debug('[View]', 'Completed'); });
 
-    let root = new TodoList((e) => { return this.emit(e); }, viewEvents),
-        widget = new RxWidget(viewEvents, (opts) => { return root.render(opts); }, initial);
+    let root = new TodoList((e) => { return this.emit(e); }, this.state),
+        widget = new RxWidget(this.state, (opts) => { return root.render(opts); }, initial);
 
     this.container.appendChild(widget.init());
   }
